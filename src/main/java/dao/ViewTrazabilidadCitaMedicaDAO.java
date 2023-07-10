2fbe690f53d1c03b87a6c3f05e2c91c2b6d14fdc
Carlos Enrique Chinga Ramos <chinga_carlos@hotmail.com>
2023-07-10 12:27:20 -0500

    Se modifico la consulta

---
+++ src/main/java/dao/ViewTrazabilidadCitaMedicaDAO.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import com.google.gson.Gson;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class ViewTrazabilidadCitaMedicaDAO {
+     private static EntityManagerFactory getEntity() {
+        try {
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("com.clinicasb.persis");
+            return emf;
+        } catch (Exception e) {
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("com.clinicasb.persis");
+            return emf;
+        }
+
+    }
+
+   
+/*
+    public static List<ViewTrazabilidadCitaMedica> listar(Date startDate, Date endDate) {
+        try {
+            EntityManager em = getEntity().createEntityManager();
+            Query q = em.createNamedQuery("ViewTrazabilidadLa.findByFechaFiltroBeetwen");
+            q.setParameter("startDate", startDate);
+            q.setParameter("endDate", endDate);
+            List<ViewTrazabilidadCitaMedica> lista = q.getResultList();
+            return lista;
+        } catch (Exception ex) {
+            System.out.println("Errorr en listar");
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
+            Date filtroInicio = formato.parse("2023-06-30");
+            Date filtroFin = formato.parse("2023-06-30");
+            List<ViewTrazabilidadCitaMedica> lista = ViewTrazabilidadCitaMedicaDAO.listar(filtroInicio, filtroFin);
+            
+            /*Gson g= new Gson();
+            String resultado= g.toJson(lista);
+            resultado="{\"data\":" + resultado+"}";
+            System.out.println(resultado);
+            int i=0;
+            for (ViewTrazabilidadCitaMedica o : lista) {
+                System.out.println(o.getSecuencua() + " - " + o.getPaciente()+" - " + o.getDiagnostico1() + " - " + o.getFechaCita());
+                i++;
+            }
+            
+              /* Gson g = new Gson();
+                String resultado = g.toJson(lista);
+                resultado = "{\"data\":" + resultado + "}";
+                System.out.println(resultado);
+            System.out.println("total" + i);
+        } catch (Exception ex) {
+            System.out.println("Error");
+        }
+
+    }*/
+}
